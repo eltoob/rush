c5abac34e16a136f05a01a16902532c818f94b4d
Hal Rogers <hal.c.rogers@gmail.com>
2016-09-09 17:38:05 -0400

    Fixes some issues around reading/writing config
    Configuration values were sometimes being passed in as a
    string and sometimes as a symbol which caused the client
    initialization to behave a little strangely. This ensures
    the keys in the configuration hash are always symbols
    whether they are passed in as strings or not

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -7,6 +7,17 @@ describe Rush do
         client = Rush::Client.new(sandbox: true)
         expect(client.api_uri).to eq "https://sandbox-api.uber.com/v1/"
       end
+
+      it "overrides the default configuration" do
+        Rush.configure do |config|
+          config.client_id = "client_id"
+          config.sandbox = true
+          config.access_token = "access_token"
+        end
+        client = Rush::Client.new(client_id: "other_client_id")
+
+        expect(client.client_id).to eq "other_client_id"
+      end
     end
   end
 end
